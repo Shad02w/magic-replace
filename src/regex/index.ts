f322d4a1f552c1e00e62ca343114d8bcca193bd3
Shad02w <alvistse123@gmail.com>
2024-12-26 18:33:25 -0500

    refactor!: throw type error when either prefix or suffix is empty

--- src/regex/index.ts
+++ src/regex/index.ts
@@ -8,6 +8,7 @@ export function createMatchingRegex(
     [prefix, suffix]: [string, string],
     mapper: Record<string, string>,
 ) {
+    assertPattern([prefix, suffix]);
     const keys = Object.keys(mapper).map((key) => escapeChar(key));
 
     const union = `${escapeChar(prefix)}(${keys.join("|")})${escapeChar(suffix)}`;
@@ -15,10 +16,25 @@ export function createMatchingRegex(
 }
 
 export function createExtractRegex([prefix, suffix]: [string, string]) {
+    assertPattern([prefix, suffix]);
     const escapedPrefix = escapeChar(prefix);
     const escapedSuffix = escapeChar(suffix);
     return new RegExp(
         `${escapedPrefix}((?:(?!${escapedPrefix}|${escapedSuffix}).)+)${escapedSuffix}`,
         "g",
     );
+}
+
+function assertPattern([prefix, suffix]: [string, string]) {
+    if (prefix.length === 0 && suffix.length === 0) {
+        throw new TypeError("prefix and suffix cannot be empty string");
+    }
+
+    if (prefix.length === 0) {
+        throw new TypeError("prefix cannot be empty string");
+    }
+
+    if (suffix.length === 0) {
+        throw new TypeError("suffix cannot be empty string");
+    }
 }
